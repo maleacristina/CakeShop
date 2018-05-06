e2d4ca170809627e08c838634d7ca6153f6cafad
maleacristina <maleacristinaecaterina@gmail.com>
2018-05-06 17:59:37 +0300

    Add DataContext and connection to the database

---
+++ MiroBello/MiroBello/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MiroBello.Models;
+
+namespace MiroBello.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options ):base(options) { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductsOnBill> ProductsOnBills { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+
+    }
+}
--- MiroBello/MiroBello/Startup.cs
+++ MiroBello/MiroBello/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MiroBello.Data;
 
 namespace MiroBello
 {
@@ -23,6 +25,7 @@ namespace MiroBello
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
