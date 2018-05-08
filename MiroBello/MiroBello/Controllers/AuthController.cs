8f79a1f0b41a515055c97b67f9ed1a173ca748de
maleacristina <maleacristinaecaterina@gmail.com>
2018-05-08 15:06:01 +0300

    Using data transfer object for register and login system

--- MiroBello/MiroBello/Controllers/AuthController.cs
+++ MiroBello/MiroBello/Controllers/AuthController.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using MiroBello.Data;
+using MiroBello.Dtos;
 using MiroBello.Models;
 
 namespace MiroBello.Controllers
@@ -12,30 +18,63 @@ namespace MiroBello.Controllers
     public class AuthController : Controller
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
+            _config = config;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string firstname, string lastname, string address, string phonenumber, string email,string password)
+        public async Task<IActionResult> Register([FromBody]ClientForRegisterDto clientForRegisterDto)
         {
+            clientForRegisterDto.Email = clientForRegisterDto.Email.ToLower();
+
+            if (await _repo.UserExists(clientForRegisterDto.Email))
+                ModelState.AddModelError("Email","Client is already existing!");
+
             //validate request
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-            email=email.ToLower();
-
-            if (await _repo.UserExists(email))
-                return BadRequest(("Client is already existing!"));
-
+            
             var clientToCreate = new ClientAccount
             {
-                Email = email
+                Email = clientForRegisterDto.Email
             };
-            var createMail = await _repo.Register(firstname,lastname,address,phonenumber,clientToCreate, password);
+            var createMail = await _repo.Register(clientForRegisterDto.FirstName, clientForRegisterDto.LastName, clientForRegisterDto.Address, clientForRegisterDto.PhoneNumber, clientToCreate, clientForRegisterDto.Password);
 
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody]ClientForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Email.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            // generate token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.Email)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { tokenString });
         }
 
     }
---
+++ MiroBello/MiroBello/Dtos/ClientForRegisterDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiroBello.Dtos
+{
+    public class ClientForRegisterDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify a password between 4 and 8 characters")]
+        public string Password { get; set; }
+    }
+}
